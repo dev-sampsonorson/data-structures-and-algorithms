8d8b82e0704d290092974da85c366e7ab6b09ffd
Sampson Orson Jackson <sampson.orson@gmail.com>
2021-05-03 19:26:09 +0100

    added Empty.cs and note on how computer works

---
+++ Code/DataStructureAndAlgorithm/Solutions/Solution6.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructureAndAlgorithm.Core;
+
+namespace DataStructureAndAlgorithm.Solutions {
+    public class Solution6 : ISolution {
+        public void Run() {
+            
+        }
+    }
+}
