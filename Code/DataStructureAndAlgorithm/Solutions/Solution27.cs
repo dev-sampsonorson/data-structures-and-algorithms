f2d5f46bf109552cdd5760dce2ffdaa688a5f8b3
Sampson Orson Jackson <sampson.orson@gmail.com>
2021-06-14 22:39:10 +0100

    added iterative implementatio for reverse string with recursion

--- Code/DataStructureAndAlgorithm/Solutions/Solution27.cs
+++ Code/DataStructureAndAlgorithm/Solutions/Solution27.cs
@@ -19,8 +19,8 @@ namespace DataStructureAndAlgorithm.Solutions {
             public Input27(string value) => this.Value = value;
         }
 
-        private class First : ISolve<Input27, string> {
-            public string Description => "First Implementation";
+        private class Recursive : ISolve<Input27, string> {
+            public string Description => "Recursive Implementation";
 
             public string Implementation(Input27 input) {
                 return this.ReverseString(input.Value);
@@ -30,6 +30,31 @@ namespace DataStructureAndAlgorithm.Solutions {
                 if (value.Length == 1) return value;
 
                 return ReverseString(value.Substring(1)) + value.Substring(0, 1);
+            }
+        }
+
+        private class Iterative : ISolve<Input27, string> {
+            public string Description => "Iterative Implementation";
+
+            public string Implementation(Input27 input) {
+                char[] value = input.Value.ToCharArray();
+                //string[] value = new[] { input.Value }; ;
+                int len = value.Length;
+
+                int left = 0, right = len - 1;
+
+                while(left < right) {
+                    //var temp = value[left];
+                    //value[left] = value[right];
+                    //value[right] = temp;
+
+                    (value[left], value[right]) = (value[right], value[left]);
+
+                    left += 1;
+                    right -= 1;
+                }
+
+                return string.Join("", value);
             }
         }
     }
